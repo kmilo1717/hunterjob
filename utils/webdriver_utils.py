6474bc2282d0bd1f1c207138ec98e182d3663d5c
Camilo Reyes <xikmiloix.com@gmail.com>
2025-04-19 03:20:35 -0500

    Fix: firefox options

--- utils/webdriver_utils.py
+++ utils/webdriver_utils.py
@@ -20,11 +20,11 @@ def get_firefox_options():
     load_dotenv()
 
     options = FirefoxOptions()
-    options.add_argument("start-maximized")
-    options.add_argument("window-size=1400x900")
+    options.add_argument("--width=1400")
+    options.add_argument("--height=900")
+
     if os.getenv('APP_ENV') == 'production':
         options.add_argument("--headless")
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
+        options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+
     return options
